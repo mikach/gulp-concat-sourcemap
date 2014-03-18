7fa50a559b6b9055690ed733f77f826fdadd3e2e
mikach <mikach27@gmail.com>
2014-03-18 11:17:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+gulp-concat-sourcemap
+=====================
+
+Concatenate files and generate a source map file.
+
+Based on https://github.com/wearefractal/gulp-concat
+
+### Usage
+
+```javascript
+var concat = require('gulp-concat-sourcemap');
+
+gulp.task('concat', function() {
+    gulp.src(['file1.js', './js/*.js', 'file2.js'])
+        .pipe(concat('all.js', options))
+        .pipe(gulp.dest('./dist/'));
+});
+```
+
+### Options
+
+`options.sourcesContent`
+An optional flag that tells the source map generator whether or not to include all original sources in the map.
+
+`options.sourceRoot`
+An optional root for all relative URLs in the source map.
---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var through = require('through');
+
+var PluginError = require('gulp-util').PluginError;
+var File = require('gulp-util').File;
+
+var SourceMapConsumer = require('source-map').SourceMapConsumer;
+var SourceMapGenerator = require('source-map').SourceMapGenerator;
+var SourceNode = require('source-map').SourceNode;
+
+module.exports = function(fileName, opts) {
+    if (!fileName) {
+        throw new PluginError('gulp-concat-sourcemap', 'Missing fileName option for gulp-concat-sourcemap');
+    }
+
+    opts = opts || {};
+
+    var firstFile = null;
+
+    var sourceNode = new SourceNode();
+
+    function bufferContents(file) {
+        if (file.isNull()) return; // ignore
+        if (file.isStream()) return this.emit('error', new PluginError('gulp-concat-sourcemap', 'Streaming not supported'));
+
+        if (!firstFile) firstFile = file;
+
+        var rel = path.relative(file.cwd, file.path).replace('\\', '/');
+
+        file.contents.toString('utf8').split('\n').forEach(function(line, j){
+            sourceNode.add(new SourceNode(j + 1, 0, rel, line + '\n'));
+        });
+        sourceNode.add('\n');
+
+        if (opts.sourcesContent) {
+            sourceNode.setSourceContent(file.relative, file.contents.toString('utf8'));
+        }
+    }
+
+    function endStream(){
+        if (!firstFile) return;
+
+        var contentPath = path.join(firstFile.base, fileName),
+            mapPath = contentPath + '.map';
+
+        if (/\.css$/.test(fileName)) {
+            sourceNode.add('/*# sourceMappingURL=' + fileName + '.map' + ' */');
+        } else {
+            sourceNode.add('//# sourceMappingURL=' + fileName + '.map');
+        }
+
+        var codeMap = sourceNode.toStringWithSourceMap({
+            file: fileName,
+            sourceRoot: opts.sourceRoot || ''
+        });
+
+        var sourceMap = SourceMapGenerator
+                                .fromSourceMap( new SourceMapConsumer( codeMap.map.toJSON() ) )
+                                .toJSON();
+                                
+        sourceMap.file = path.basename(sourceMap.file);
+
+        var contentFile = new File({
+            cwd: firstFile.cwd,
+            base: firstFile.base,
+            path: contentPath,
+            contents: new Buffer(codeMap.code)
+        });
+
+        var mapFile = new File({
+            cwd: firstFile.cwd,
+            base: firstFile.base,
+            path: mapPath,
+            contents: new Buffer(JSON.stringify(sourceMap, null, '  '))
+        });
+
+        this.emit('data', contentFile);
+        this.emit('data', mapFile);
+        this.emit('end');
+    }
+
+    return through(bufferContents, endStream);
+};
