7fd55a12a0b6ed83b2cefb295d64e941e9b722c7
mikach <mikach27@gmail.com>
2014-03-18 17:30:44 +0200

    Add tests

---
+++ test/test.js
@@ -0,0 +1,54 @@
+var expect = require('chai').expect;
+
+var Stream = require('stream');
+
+var File = require('gulp-util').File;
+
+var concat = require('..');
+
+describe('gulp-concat-sourcemap', function() {
+    it('should be a stream', function() {
+        expect(concat('file.js')).to.be.instanceOf(Stream);
+    });
+
+    it('should concat files', function(done) {
+        var file1 = new File({
+            cwd: 'test',
+            base: '/test',
+            path: 'test/file1.js',
+            contents: new Buffer('console.log(\'Hello\');')
+        });
+
+        var file2 = new File({
+            cwd: 'test',
+            base: '/test',
+            path: 'test/file2.js',
+            contents: new Buffer('console.log(\'World\');')
+        });
+
+        var stream = concat('file.js');
+
+        var contentFile;
+        stream.on('data', function(newFile) {
+            if (!contentFile) { // contentFile
+                contentFile = newFile;
+                expect(String(newFile.contents)).to.be.equal(
+                    'console.log(\'Hello\');' +
+                    '\n\nconsole.log(\'World\');\n\n' +
+                    '//# sourceMappingURL=file.js.map'
+                );
+            } else { // mapFile
+                expect(String(newFile.contents)).to.be.equal(
+                    '{\n  \"version\": 3,\n  \"file\": \"file.js\",' + 
+                    '\n  \"sources\": [\n    \"file1.js\",\n    \"file2.js\"\n  ]' +
+                    ',\n  \"names\": [],\n  \"mappings\": \"AAAA;;ACAA\"\n}'
+                );
+                done();
+            }
+        });
+
+        stream.write(file1);
+        stream.write(file2);
+        stream.end();
+    });
+});
